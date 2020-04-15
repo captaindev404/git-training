050b4181793f1d931e2c36bf5872d13be0814d48
eer <emmanuel@captaindev.io>
2020-04-15 22:51:51 +0200

    check parameters

--- utils.js
+++ utils.js
@@ -3,5 +3,8 @@ const fullName = (first,last) => {
 }
 
 const formatDate = (dateString) => {
+    if(dateString == null){
+        throw new Error('datestring cannot be null')
+    }
     return new Date(dateString)
 }
