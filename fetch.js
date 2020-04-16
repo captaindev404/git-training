46321a4dfaa82fcfdac0b59f2e06d2db132477bb
Captain Dev <emmanuel@captaindev.io>
2020-04-16 14:48:37 +0200

    Integrate fetch constants

--- fetch.js
+++ fetch.js
@@ -1,5 +1,8 @@
-const postData = async (url) => {
-  return fetchData(url, {method: 'post'})
+const GET_METHOD = 'get'
+const POST_METHOD = 'post'
+
+const postData = async (url ) => {
+  return fetchData(url, { method: POST_METHOD});
 }
 
 const fetchData = async (url, options) => {
