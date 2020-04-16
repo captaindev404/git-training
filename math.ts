53c848914c1ccaa420d3c2cbc4a5b15c97b82762
Captain Dev <emmanuel@captaindev.io>
2020-04-16 15:16:11 +0200

    extract math helper

---
+++ math.ts
@@ -0,0 +1,23 @@
+export const power = (a) => {
+    return a * a
+}
+
+export const div = (a, b) => {
+    return a / b;
+}
+
+export const sum = (a,b) => {
+    return a * b;
+}
+
+export const multiply = (a,b) => {
+    return a * b
+}
+
+export const add = (a,b) => {
+    return a + b;
+}
+
+export const isPositiveNumber = (num) => {
+    return num > 0;
+}
