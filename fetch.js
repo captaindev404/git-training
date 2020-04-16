44affe50011df7041d9596558c322c6a374167e4
Captain Dev <emmanuel@captaindev.io>
2020-04-16 14:59:18 +0200

    test 2

--- fetch.js
+++ fetch.js
@@ -1,15 +1,20 @@
+const PUT_METHOD = 'put';
 const GET_METHOD = 'get'
 const DELETE_METHOD = 'delete';
 const POST_METHOD = 'post'
 
 const postData = async (url ) => {
   return fetchData(url, { method: POST_METHOD});
-}
 
+}
 const deleteData = async (url) => {
   return fetchData(url, {method: DELETE_METHOD})
-}
 
+}
 const fetchData = async (url, options) => {
   return fetch(url,options)
+
+}
+const putData = async (url) => {
+  return fetchData(url, { method: PUT_METHOD})
 }
