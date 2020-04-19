12a4b68870e902b602562d4642d1d1ecc1c418cc
Captain Dev <emmanuel@captaindev.io>
2020-04-19 17:37:41 +0200

    fout le bazar

--- fetch.js
+++ fetch.js
@@ -1,21 +1,6 @@
 import {DELETE_METHOD, OPTIONS_METHOD, POST_METHOD, PUT_METHOD} from './constats';
 
-const postData = async (url ) => {
 
-  return fetchData(url, { method: POST_METHOD});
-}
-const deleteData = async (url) => {
-
-  return fetchData(url, {method: DELETE_METHOD})
-}
-const fetchData = async (url, options) => {
-
-  return fetch(url,options)
-}
-const putData = async (url) => {
-  return fetchData(url, { method: PUT_METHOD})
-
-}
 const optionsData = async (url) => {
   return fetchData(url, { method: OPTIONS_METHOD})
 }
